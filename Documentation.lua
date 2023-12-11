21b413cd438f12e3a8b6edd3fb90df23ea930cf5
Henry Huy <126648750+Huy1912011@users.noreply.github.com>
2023-12-11 18:06:28 +0700

    Update Documentation.lua

--- Documentation.lua
+++ Documentation.lua
@@ -1,5 +1,10 @@
 if game.PlaceId == 623823800 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, IntroText = "Vip", SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 end
 OrionLib:Init()
