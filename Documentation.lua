10f4a612c6cee5adc096e139a3c76853710875a2
Henry Huy <126648750+Huy1912011@users.noreply.github.com>
2023-12-11 17:17:02 +0700

    Documentation.lua

--- Documentation.lua
+++ Documentation.lua
@@ -2,8 +2,8 @@ local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shle
 
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-local Tab = Window:MakeTab({
+local Tab = Window:MakeTab(
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
-})
+)
